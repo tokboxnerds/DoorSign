c29916115ff9e0a0601fb2624bbabe60be592e2b
Patrick Quinn-Graham <make-contact@pftqg.com>
2014-06-26 10:05:11 -0700

    Initial Commit

---
+++ DoorSign/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoorSign
+//
+//  Created by Patrick Quinn-Graham on 26/06/2014.
+//  Copyright (c) 2014 Patrick Quinn-Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoorSignTests/DoorSignTests.m
@@ -0,0 +1,39 @@
+//
+//  DoorSignTests.m
+//  DoorSignTests
+//
+//  Created by Patrick Quinn-Graham on 26/06/2014.
+//  Copyright (c) 2014 Patrick Quinn-Graham. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DoorSignTests : XCTestCase
+
+@end
+
+@implementation DoorSignTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
